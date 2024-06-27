dcbc1b8a4f978a960a7e58b7e9224249af8bc8cc
lyuheng <46214553+lyuheng@users.noreply.github.com>
2024-06-27 17:21:23 +0800

    Add files via upload

--- app_gmatch/run.cpp
+++ app_gmatch/run.cpp
@@ -28,9 +28,9 @@ int main(int argc, char *argv[])
     
     std::string query_path = "/home/lyuheng/TKDE-revise/TthinkerQ/data/query_graphs";
     FormatGraph g(file_path);
-    // g.Preprocess();
+    g.Preprocess();
     // g.writeGraphFile(file_path);
-    g.sampleQueryGraph(query_path);
+    // g.sampleQueryGraph(query_path);
 #else
     // char *file_path = "/home/lyuan/graph_data/gmatch_data/GSE1730.graph";
     int num_compers = 32;
