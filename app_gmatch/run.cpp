a6179f1ec69090136eac2955da494247514a315b
lyuheng <46214553+lyuheng@users.noreply.github.com>
2024-06-27 17:04:18 +0800

    Add files via upload

--- app_gmatch/run.cpp
+++ app_gmatch/run.cpp
@@ -17,24 +17,26 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
+    std::string file_path = "/home/lyuheng/TKDE-revise/TthinkerQ/data/data_graphs/com-amazon.ungraph.graph";
+    
 #ifdef GEN_GRAPH
     // char *file_path = "/home/lyuan/graph_data/GSE10158_q";
     // FormatGraph fg;
     // fg.loadGraphFromFile(file_path);
     // fg.graphDump();
 
-    std::string file_path = "xxx";
-    std::string query_path = "xxx";
+    
+    std::string query_path = "/home/lyuheng/TKDE-revise/TthinkerQ/data/query_graphs";
     FormatGraph g(file_path);
-    g.Preprocess();
-    g.writeGraphFile(file_path);
+    // g.Preprocess();
+    // g.writeGraphFile(file_path);
     g.sampleQueryGraph(query_path);
 #else
-    char *file_path = "/home/lyuan/graph_data/gmatch_data/GSE1730.graph";
+    // char *file_path = "/home/lyuan/graph_data/gmatch_data/GSE1730.graph";
     int num_compers = 32;
 
     GMWorker worker(num_compers);
-    worker.load_data(file_path);
+    worker.load_data(file_path.c_str());
 
     worker.run();
 #endif
