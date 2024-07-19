7f3e67909b1a7949dde75cab5ba54699b80aaf78
lyuheng <46214553+lyuheng@users.noreply.github.com>
2024-07-19 15:50:43 +0800

    Add files via upload

--- app_maximalclique/mc.h
+++ app_maximalclique/mc.h
@@ -41,6 +41,8 @@ struct MCQuery
     ui src;
     vector<ULL> counters;
 
+    struct timeb start_t, end_t;
+
     MCQuery()
     {
         counters.assign(32, 0);
@@ -65,11 +67,14 @@ void ordered_insert(vector<ui> & newR, ui to_add)
 class MCComper: public Comper<MCTask, MCQuery>
 {
 public:
+    ULL counter;
+
     MCComper() {}
 
     virtual bool toQuery(string& line, MCQuery& q)
     {
         q.src = stoi(line);
+        ftime(&q.start_t);
         return true;
     }
 
@@ -104,9 +109,10 @@ public:
         if (P.size() == 0 && X.size() == 0)
         {
             // report R;
-            cout << "************** ";
-            for (auto x: R) cout << x << ' ';
-            cout << endl;
+            // cout << "************** ";
+            // for (auto x: R) cout << x << ' ';
+            // cout << endl;
+            return;
         }
         for (ui i=0; i<P.size(); ++i)
         {
@@ -136,7 +142,6 @@ public:
                 t->context.X = move(newX);
                 add_task(t);
             }
-
             X.push_back(v);
         }
     }
@@ -144,7 +149,25 @@ public:
     virtual void compute(ContextT &context, MCQuery &q)
     {
         ftime(&data_graph.gtime_start[thread_id]);
+        counter = q.counters[thread_id];
         BK(context.R, context.P, context.X);
+        q.counters[thread_id] = counter;
+    }
+
+    virtual bool postprocess(GMQuery &q)
+    {
+        
+        ftime(&q.end_t);
+        double totaltime = q.end_t.time-q.start_t.time+(double)(q.end_t.millitm-q.start_t.millitm)/1000;
+        cout<<"Query "<<get_queryID()<<" total time: "<<totaltime<<endl;
+        ULL total_results = 0;
+        for(ui i=0; i<32; i++)
+        {
+            total_results += q.counters[i];
+        }
+        cout<<"Query "<<get_queryID()<<" total count: "<< total_results <<endl;
+
+        return false;
     }
 };
 
@@ -165,4 +188,3 @@ public:
         data_graph.loadGraphFromFile(fp);
     }
 };
-
