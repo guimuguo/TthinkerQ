da23cc1da085f4049656becae751f03cbc4da798
lyuheng <46214553+lyuheng@users.noreply.github.com>
2024-07-19 14:55:14 +0800

    Add files via upload

--- app_maximalclique/run.cpp
+++ app_maximalclique/run.cpp
@@ -17,8 +17,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-    std::string file_path = "/home/lyuheng/TKDE-revise/TthinkerQ/data/data_graphs/amazon/com-amazon.ungraph.graph";
-    char * file_path_c    = "/home/lyuheng/TKDE-revise/TthinkerQ/data/data_graphs/amazon/com-amazon.ungraph.graph";
+    // std::string file_path = "/home/lyuheng/TKDE-revise/TthinkerQ/data/data_graphs/amazon/com-amazon.ungraph.graph";
+    char * file_path_c    = "/home/lyuheng/TKDE-revise/TthinkerQ/app_maximalclique/test_graph.graph";
 
     int num_compers = 32;
 
